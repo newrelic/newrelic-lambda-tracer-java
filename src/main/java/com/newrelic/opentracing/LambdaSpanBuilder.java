31b5ab8369daa5aef837bcb170a271902cb0ef1a
jkeller <jkeller@newrelic.com>
2020-07-22 16:29:54 -0700

    Add license file headers to all source files.

--- src/main/java/com/newrelic/opentracing/LambdaSpanBuilder.java
+++ src/main/java/com/newrelic/opentracing/LambdaSpanBuilder.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2020 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package com.newrelic.opentracing;
 
 import com.newrelic.opentracing.state.PrioritySamplingState;
