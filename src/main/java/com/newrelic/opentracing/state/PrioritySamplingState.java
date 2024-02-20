cbb581a756faa360414267c39b69a2ad76601fcf
Andre Onuki <aonuki@newrelic.com>
2024-02-20 15:34:29 -0500

    Preventing NPE when priority in DT payload is null

--- src/main/java/com/newrelic/opentracing/state/PrioritySamplingState.java
+++ src/main/java/com/newrelic/opentracing/state/PrioritySamplingState.java
@@ -5,6 +5,7 @@
 
 package com.newrelic.opentracing.state;
 
+import com.newrelic.opentracing.dt.DistributedTracePayloadImpl;
 import com.newrelic.opentracing.util.DistributedTraceUtil;
 
 public class PrioritySamplingState {
@@ -14,6 +15,14 @@ public class PrioritySamplingState {
     public static PrioritySamplingState setSampledAndGeneratePriority(boolean computeSampled) {
         final float priority = DistributedTraceUtil.nextTruncatedFloat() + (computeSampled ? 1.0f : 0.0f);
         return new PrioritySamplingState(priority, computeSampled);
+    }
+
+    public static PrioritySamplingState create(DistributedTracePayloadImpl dtPayload) {
+        if (dtPayload.getPriority() == null) {
+            return setSampledAndGeneratePriority(dtPayload.isSampled());
+        } else {
+            return new PrioritySamplingState(dtPayload.getPriority(), dtPayload.isSampled());
+        }
     }
 
     public PrioritySamplingState(float priority, boolean sampled) {
